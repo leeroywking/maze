282c0d91f6cda14a05adce2dfb08c1f5f169ed7d
Lee-Roy King <leeroywking@gmail.com>
2021-03-11 10:48:30 -0800

    fixed scaling issue

--- maze_primitives/main.py
+++ maze_primitives/main.py
@@ -1,8 +1,11 @@
+#!/bin/usr/python3
+
 from maze import Maze
 from draw_maze import draw_maze
 
 if __name__ == "__main__":
-    maze = Maze("25x25")
+    size = "10x20"
+    maze = Maze(size)
     maze.make_maze_mazey()
     # maze.show_maze_data()
-    draw_maze(maze.walls)
+    draw_maze(maze.walls, size, 500,1000)
